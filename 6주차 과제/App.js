4082c03731e514fa7be10fe3d79d12219173a134
sangrae2325 <106955624+sangrae2325@users.noreply.github.com>
2025-05-29 17:26:53 +0900

    6주차 과제

--- 6주차 과제/App.js
+++ 6주차 과제/App.js
@@ -1,1 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewsList from "./components/NewsList";
+import NewsDetail from "./components/NewsDetail";
+import CategoryFilter from "./components/CategoryFilter";
+import { fetchNewsByCategory } from "./Api";
+
+function Home() {
+  const [category, setCategory] = useState("general");
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchNewsByCategory(category).then(setArticles);
+  }, [category]);
+
+  return (
+    <div>
+      <CategoryFilter category={category} setCategory={setCategory} />
+      <NewsList articles={articles} />
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/article/:index" element={<NewsDetail />} />
+      </Routes>
+    </Router>
+  );
+}
 
